933acb3fd15036591bafd6b89b9703d04f2b3f5c
justdk06 <justdk06@gmail.com>
2025-06-16 22:05:50 +0530

    commit all the file

--- 06_Lecture-Even-Loop/calculator/sum.js
+++ 06_Lecture-Even-Loop/calculator/sum.js
@@ -1,27 +1,26 @@
-const sumRequestHandler = (req, res) =>{
+const sumRequestHandler = (req, res) => {
   console.log('In Sum Request Handler', req.url);
   const body = [];
-  let result;
 
-  req.on('data', chunk =>body.push(chunk));
-  req.on('end', () =>{
-    const bodyStr =  Buffer.concat(body).toString();
+  req.on('data', chunk => body.push(chunk));
+  req.on('end', () => {
+    const bodyStr = Buffer.concat(body).toString();
     const params = new URLSearchParams(bodyStr);
     const bodyObj = Object.fromEntries(params);
     const result = Number(bodyObj.first) + Number(bodyObj.second);
-    console.log(result)
+    console.log(result);
 
     res.setHeader("Content-Type", 'text/html');
     res.write(`
       <html>
         <head><title>Practise Set</title></head>
         <body>
-          <h1>Your Sum is 78 ${result}</h1>
+          <h1>Your Sum is ${result}</h1>
         </body>
       </html>
     `);
     return res.end();
   });
-}
+};
 
 exports.sumRequestHandler = sumRequestHandler;
